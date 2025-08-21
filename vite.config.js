6f83cb9e4665b32cd7c2d7e2a02e2a1299a96afc
jaisonschmidt <jaison.schmidt@invillia.com>
2025-08-21 14:53:32 -0300

    fix: update base path in Vite config for local development

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/slide-notes/', // necessário para GitHub Pages
+  base: '/', // ajustado para rodar na raiz do projeto localmente
   plugins: [react()],
 })
